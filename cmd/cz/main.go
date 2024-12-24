42869601b2ad009e1319895132c195076e888f1a
yerik.ussayev <yerik.ussayev@tele2.kz>
2024-12-24 16:06:44 +0500

    ref(committer): move generating commit message to package

--- cmd/cz/main.go
+++ cmd/cz/main.go
@@ -1,32 +1,14 @@
 package main
 
 import (
+	"cz/internal/committer"
 	"cz/internal/git"
 	"fmt"
 	"github.com/boostgo/lite/log"
-	"github.com/boostgo/lite/system/trace"
-	"github.com/manifoldco/promptui"
 	"os"
 )
 
-// CommitType represents a type of commit
-type CommitType struct {
-	Label       string
-	Description string
-}
-
-var commitTypes = []CommitType{
-	{Label: "feat", Description: "A new feature"},
-	{Label: "fix", Description: "A bug fix"},
-	{Label: "docs", Description: "Documentation only changes"},
-	{Label: "ref", Description: "A code change that neither fixes a bug nor adds a feature"},
-	{Label: "perf", Description: "A code change that improves performance"},
-	{Label: "test", Description: "Adding missing tests or correcting existing tests"},
-	{Label: "ci", Description: "Changes to our CI configuration files and scripts"},
-}
-
 func main() {
-	trace.IAmMaster()
 	log.PrettyLog()
 
 	if len(os.Args) < 2 || os.Args[1] != "commit" {
@@ -34,88 +16,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	commitMsg, err := generateCommitMessage()
+	// generate commit message
+	commitMsg, err := committer.GenerateMessage()
 	if err != nil {
 		log.Error().Err(err).Msg("Generate commit message")
 		os.Exit(1)
 	}
 
+	// execute git commit
 	if err = git.Commit(commitMsg); err != nil {
 		log.Error().Err(err).Msg("Commit")
 		os.Exit(1)
 	}
-
-	log.Info().Msg("cz")
-}
-
-func generateCommitMessage() (string, error) {
-	// 1. Select commit type
-	typeSelect := promptui.Select{
-		Label: "Select commit type",
-		Items: commitTypes,
-		Templates: &promptui.SelectTemplates{
-			Label:    "{{ . }}?",
-			Active:   "\U0001F449 {{ .Label | cyan }} - {{ .Description | cyan }}",
-			Inactive: "  {{ .Label | white }} - {{ .Description | white }}",
-			Selected: "\U0001F44D {{ .Label | green }} - {{ .Description | green }}",
-		},
-	}
-
-	typeIdx, _, err := typeSelect.Run()
-	if err != nil {
-		return "", fmt.Errorf("prompt failed: %v", err)
-	}
-
-	// 2. Enter scope (optional)
-	scopePrompt := promptui.Prompt{
-		Label:     "Scope (optional)",
-		Default:   "",
-		AllowEdit: true,
-	}
-
-	scope, err := scopePrompt.Run()
-	if err != nil {
-		return "", fmt.Errorf("prompt failed: %v", err)
-	}
-
-	// 3. Enter commit message
-	messagePrompt := promptui.Prompt{
-		Label: "Commit message",
-		Validate: func(input string) error {
-			if len(input) < 3 {
-				return fmt.Errorf("commit message must be at least 3 characters")
-			}
-			return nil
-		},
-	}
-
-	message, err := messagePrompt.Run()
-	if err != nil {
-		return "", fmt.Errorf("prompt failed: %v", err)
-	}
-
-	// 4. Format commit message
-	commitMsg := commitTypes[typeIdx].Label
-	if scope != "" {
-		commitMsg += "(" + scope + ")"
-	}
-	commitMsg += ": " + message
-
-	// 5. Optional longer description
-	bodyPrompt := promptui.Prompt{
-		Label:     "Longer description (optional, press Enter to skip)",
-		Default:   "",
-		AllowEdit: true,
-	}
-
-	body, err := bodyPrompt.Run()
-	if err != nil {
-		return "", fmt.Errorf("prompt failed: %v", err)
-	}
-
-	if body != "" {
-		commitMsg += "\n\n" + body
-	}
-
-	return commitMsg, nil
 }
--- internal/committer/committer.go
+++ internal/committer/committer.go
@@ -1,1 +1,95 @@
 package committer
+
+import (
+	"fmt"
+	"github.com/manifoldco/promptui"
+)
+
+// CommitType represents a type of commit
+type CommitType struct {
+	Label       string
+	Description string
+}
+
+var commitTypes = []CommitType{
+	{Label: "feat", Description: "A new feature"},
+	{Label: "fix", Description: "A bug fix"},
+	{Label: "wip", Description: "Work In Progress"},
+	{Label: "ref", Description: "A code change that neither fixes a bug nor adds a feature"},
+	{Label: "docs", Description: "Documentation only changes"},
+	{Label: "perf", Description: "A code change that improves performance"},
+	{Label: "test", Description: "Adding missing tests or correcting existing tests"},
+	{Label: "ci", Description: "Changes to our CI configuration files and scripts"},
+}
+
+func GenerateMessage() (message string, err error) {
+	// 1. Select commit type
+	typeSelect := promptui.Select{
+		Label: "Select commit type",
+		Items: commitTypes,
+		Templates: &promptui.SelectTemplates{
+			Label:    "{{ . }}?",
+			Active:   "\U0001F449 {{ .Label | cyan }} - {{ .Description | cyan }}",
+			Inactive: "  {{ .Label | white }} - {{ .Description | white }}",
+			Selected: "\U0001F44D {{ .Label | green }} - {{ .Description | green }}",
+		},
+	}
+
+	typeIdx, _, err := typeSelect.Run()
+	if err != nil {
+		return message, fmt.Errorf("prompt failed: %v", err)
+	}
+
+	// 2. Enter scope (optional)
+	scopePrompt := promptui.Prompt{
+		Label:     "Scope (optional)",
+		Default:   "",
+		AllowEdit: true,
+	}
+
+	scope, err := scopePrompt.Run()
+	if err != nil {
+		return message, fmt.Errorf("prompt failed: %v", err)
+	}
+
+	// 3. Enter commit message
+	messagePrompt := promptui.Prompt{
+		Label: "Commit message",
+		Validate: func(input string) error {
+			if len(input) < 3 {
+				return fmt.Errorf("commit message must be at least 3 characters")
+			}
+			return nil
+		},
+	}
+
+	message, err = messagePrompt.Run()
+	if err != nil {
+		return "", fmt.Errorf("prompt failed: %v", err)
+	}
+
+	// 4. Format commit message
+	commitMsg := commitTypes[typeIdx].Label
+	if scope != "" {
+		commitMsg += "(" + scope + ")"
+	}
+	commitMsg += ": " + message
+
+	// 5. Optional longer description
+	bodyPrompt := promptui.Prompt{
+		Label:     "Longer description (optional, press Enter to skip)",
+		Default:   "",
+		AllowEdit: true,
+	}
+
+	body, err := bodyPrompt.Run()
+	if err != nil {
+		return message, fmt.Errorf("prompt failed: %v", err)
+	}
+
+	if body != "" {
+		commitMsg += "\n\n" + body
+	}
+
+	return commitMsg, nil
+}
